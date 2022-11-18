b35bb63d8ad496a7dc95075402ae9016f19e7b9f
riverjoy <tosakarinmylove@gmail.com>
2022-11-18 17:47:06 +0800

    money页面numberPad布局wanc

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -71,6 +71,59 @@ const TypeSection = styled.section`
   }
   }
 `
+const NumberPadSection =styled.section`
+  >.output{
+    font-family: Consolas , monospace;
+    font-size: 36px;
+    color: #333;
+    line-height: 72px;
+    text-align: right;
+    padding-right: 16px;
+  }
+  >.pad{
+  button{
+    float: left;
+    font-size: 18px;
+    width: 25%;
+    height: 64px;
+    &.ok{
+      height: 128px;
+      float:right;
+    }
+    &.zero{
+      width: 50%;
+    }
+    &:nth-child(1){
+      background:#f2f2f2;
+    }
+    &:nth-child(2),
+    &:nth-child(5) {
+      background:#E0E0E0;
+    }
+    &:nth-child(3),
+    &:nth-child(6),
+    &:nth-child(9) {
+      background:#D3D3D3;
+    }
+    &:nth-child(4),
+    &:nth-child(7),
+    &:nth-child(10) {
+      background:#C1C1C1;
+    }
+    &:nth-child(8),
+    &:nth-child(11),
+    &:nth-child(13) {
+      background:#B8B8B8;
+    }
+    &:nth-child(12) {
+      background:#9A9A9A;
+    }
+    &:nth-child(14) {
+      background:#A9A9A9;
+    }
+  } 
+  }
+`
 
 const Money=()=>{
   return (
@@ -94,7 +147,27 @@ const Money=()=>{
         <div className="selected">支出</div>
         <div>收入</div>
       </TypeSection>
-      <section>numberPad</section>
+      <NumberPadSection>
+        <div className="output">
+          100
+        </div>
+        <div className="pad clearfix">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
+          <button className="dot">.</button>
+        </div>
+      </NumberPadSection>
     </Layout>
   )
 }
