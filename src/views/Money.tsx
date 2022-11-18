46e08b5e105a4610e1ef1821091096ea991fe1a4
riverjoy <tosakarinmylove@gmail.com>
2022-11-18 17:04:49 +0800

    Money页面tags部分完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import styled from 'styled-components';
+
+const TagsSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 16px 12px;
+  ol{
+    display: flex;
+    flex-direction: row;
+    >li{
+      margin-right: 24px;
+      background: #D9D9D9; 
+      border-radius: 18px;
+      display:inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+    }
+  }
+  .button{
+    color: #999;
+    font-size: 14px;
+    border-bottom: 1px solid #666;
+    padding: 2px 4px;
+    margin-top: 18px;
+  }
+`
 
 const Money=()=>{
   return (
     <Layout>
-    <div>money</div>
+      <TagsSection>
+        <ol>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ol>
+        <button className="button">新增标签</button>
+      </TagsSection>
+      <section>notes</section>
+      <section>types</section>
+      <section>numberPad</section>
     </Layout>
   )
 }
