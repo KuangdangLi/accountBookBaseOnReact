64c859693cd6e0546f2aac83d4c23bddf419b839
riverjoy <tosakarinmylove@gmail.com>
2022-11-19 17:34:35 +0800

    TagsSection添加新增标签功能

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -11,13 +11,18 @@ const Wrapper = styled.section`
   ol{
     display: flex;
     flex-direction: row;
+    width: 100vw;
+    flex-wrap: wrap;
+    
     >li{
       margin-right: 24px;
+      margin-top: 16px;
       background: #D9D9D9; 
       border-radius: 18px;
       display:inline-block;
       padding: 3px 18px;
       font-size: 14px;
+      white-space: nowrap;
       &.selected{
         background-color: #20bf64;
         color: white;
@@ -43,16 +48,30 @@ const TagsSection = ()=>{
       setSelectedTag('')
     }
   }
+  const addTag = ()=>{
+     const newTagName = window.prompt('请输入新标签名')
+    if(newTagName === '' || newTagName === ' '){
+      window.alert('请输入标签名')
+    }else if(newTagName === null){
+      return
+    }else if(tags.indexOf(newTagName)>=0){
+      window.alert('不能输入重复的标签名')
+      return;
+    }else if(newTagName.length>4){
+      window.alert('标签名过长')
+      return;
+    }else{
+      setTags([...tags,newTagName])
+    }
+    console.log(newTagName);
+    console.log(typeof newTagName);
+  }
   return (
     <Wrapper>
     <ol>
       {tags.map(tag=><li key={tag} onClick={()=>selectTag(tag)} className={tag === selectedTag ? 'selected' : ''}>{tag}</li>)}
-      {/*<li>衣</li>*/}
-      {/*<li>食</li>*/}
-      {/*<li>住</li>*/}
-      {/*<li>行</li>*/}
     </ol>
-  <button className="button">新增标签</button>
+  <button className="button" onClick={addTag}>新增标签</button>
     </Wrapper>
   )
 }
