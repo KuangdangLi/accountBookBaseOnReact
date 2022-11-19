e13a953199af29249dc3c9262b67543f272e27f2
riverjoy <tosakarinmylove@gmail.com>
2022-11-19 17:10:54 +0800

    TagSection组件从Money中彻底抽出。TagSection添加标签选中变色效果。

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   flex-grow: 1;
   justify-content: end;
   display: flex;
@@ -17,6 +18,10 @@ const TagsSection = styled.section`
       display:inline-block;
       padding: 3px 18px;
       font-size: 14px;
+      &.selected{
+        background-color: #20bf64;
+        color: white;
+      }
     }
   }
   .button{
@@ -27,4 +32,30 @@ const TagsSection = styled.section`
     margin-top: 18px;
   }
 `
+
+const TagsSection = ()=>{
+  const [tags,setTags] = useState(['衣','食','住','行'])
+  const [selectedTag,setSelectedTag] = useState('')
+  const selectTag =(tag:string)=>{
+    if(!selectedTag || selectedTag !== tag){
+      setSelectedTag(tag)
+    }else{
+      setSelectedTag('')
+    }
+  }
+  return (
+    <Wrapper>
+    <ol>
+      {tags.map(tag=><li key={tag} onClick={()=>selectTag(tag)} className={tag === selectedTag ? 'selected' : ''}>{tag}</li>)}
+      {/*<li>衣</li>*/}
+      {/*<li>食</li>*/}
+      {/*<li>住</li>*/}
+      {/*<li>行</li>*/}
+    </ol>
+  <button className="button">新增标签</button>
+    </Wrapper>
+  )
+}
+
+
 export {TagsSection};
