60dab5dc07682d7438945f025ebda1079f7b5795
riverjoy <tosakarinmylove@gmail.com>
2022-11-20 17:59:20 +0800

    Tags组件HTML、样式完成

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import {useTags} from 'useTags';
+import styled from 'styled-components';
+import Icon from '../components/icon';
+
+const TagList = styled.ol`
+ >li{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    font-size: 16px;
+    padding: 11px 16px 12px 0;
+    margin-left: 15px;
+    text-align: center;
+    border-bottom: 1px solid #d5d5d9;
+   >svg.icon{
+     width: 20px;
+     height: 20px;
+   }
+ }
+`
+
+const Button = styled.button`
+  font-size: 16px;
+  padding: 9px 16px;
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+`
+
+const Space = styled.div`
+height: 54px;
+`
 
 const Tags=()=>{
+  const {tags,setTags} = useTags()
   return (
     <Layout>
-      <div>Tags</div>
+      <TagList>
+      {tags.map(tag=>
+        <li>
+          <span>{tag}</span>
+          <Icon name={'right'} />
+        </li>)}
+      </TagList>
+      <Space />
+      <Button>新建标签</Button>
     </Layout>
   )
 }
