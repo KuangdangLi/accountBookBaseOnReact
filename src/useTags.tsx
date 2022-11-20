0bcfa8c7cc0503102c7394e614ef6f8b29001d22
riverjoy <tosakarinmylove@gmail.com>
2022-11-21 01:50:10 +0800

    新增updateTag、removeTag、findTagIndex接口

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -13,10 +13,30 @@ const defaultTags:Tag[] =
 
 const useTags = ()=>{
   const [tags,setTags] = useState(defaultTags)
-  const findTag = (id:string)=>{
-     return  tags.filter(tag => tag.ID === parseFloat(id))[0];
+  const findTag = (id:number)=>{
+     return  tags.filter(tag => tag.ID === id)[0];
   }
-  return {tags,setTags,findTag}
+  const findTagIndex = (id:number)=>{
+    let result = -1;
+    for(let i = 0;i<tags.length;i++){
+      if(tags[i].ID === id){
+        result = i
+        break
+      }
+    }
+    return result
+  }
+  const updateTag=(id:number,obj:{name:string})=>{
+    const tagsClone = JSON.parse(JSON.stringify(tags))
+    tagsClone.splice(findTagIndex(id),1,{ID:id,...obj})
+    setTags(tagsClone)
+  }
+  const removeTag= (id:number)=>{
+    const tagsClone = JSON.parse(JSON.stringify(tags))
+    tagsClone.splice(findTagIndex(id),1)
+    setTags(tagsClone)
+  }
+  return {tags,setTags,findTag,findTagIndex,updateTag,removeTag}
 }
 
 export {useTags}
