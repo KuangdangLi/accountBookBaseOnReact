a703edc9b6e739cc81649add208ecc403f743d17
riverjoy <tosakarinmylove@gmail.com>
2022-11-20 17:31:38 +0800

    添加defaultTags变量

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,7 +1,9 @@
 import {useState} from 'react';
 
+const defaultTags = ['衣','食','住','行']
+
 const useTags = ()=>{
-  const [tags,setTags] = useState(['衣','食','住','行'])
+  const [tags,setTags] = useState(defaultTags)
   return {tags,setTags}
 }
 
