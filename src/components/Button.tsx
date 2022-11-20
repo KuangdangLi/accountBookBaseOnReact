f2ef0c0cb0053efd397630770987d9f51acd7656
riverjoy <tosakarinmylove@gmail.com>
2022-11-20 23:12:34 +0800

    抽离Button组件

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  font-size: 16px;
+  padding: 9px 16px;
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+`
+export {Button}
