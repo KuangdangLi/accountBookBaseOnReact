f4ce647366d4b259c9878170cf5982471b028c11
riverjoy <tosakarinmylove@gmail.com>
2022-11-21 01:17:33 +0800

    Input组件css修改。NotesSection.tsx增加上下padding

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,38 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Label = styled.label`
-  
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-
-    >span{
-      color: #333;
-      white-space: nowrap;
-      margin-right: 16px;
-    }
-    >input{
-      border: none;
-      display: block;
-      width: 100%;
-      height: 72px;
-      background:none;
-    }
-`
+  display:flex;
+  align-items: center;
+  > span { margin-right: 16px; white-space: nowrap; }
+  > input {
+    display:block;
+    width: 100%;
+    height: 44px;
+    line-height: 44px;
+    background: none;
+    border: none;
+  }
+`;
 
 type Props = {
-  label:String
-} & React.InputHTMLAttributes<HTMLInputElement>
-
-const Input:React.FC<Props> =(props)=>{
-  const {label,children,...rest} = props
+  label: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+const Input: React.FC<Props> = (props) => {
+  const {label, children, ...rest} = props;
   return (
     <Label>
-      <span>{label}</span>
-      <input {...rest} maxLength={10}/>
-      {/*type="text" placeholder='请输入备注' value={props.value} onChange={e=>(props.onChange(e.target.value))}*/}
+      <span>{props.label}</span>
+      <input {...rest} />
     </Label>
-  )
-}
+  );
+};
 
-export {Input}
+export {Input};
