2ec7b728f00e59d412c4d38a72f35b89f5b87db8
riverjoy <tosakarinmylove@gmail.com>
2022-11-21 21:09:04 +0800

    recordList数据持久化

---
+++ src/Hooks/useRecords.tsx
@@ -0,0 +1,24 @@
+import {useEffect, useState} from 'react';
+import {useUpdate} from './useUpdate';
+
+const useRecords = ()=>{
+  const [recordList,setRecordList] = useState<RecordItem[]>([])
+  useEffect(()=>{
+    setRecordList(JSON.parse(window.localStorage.getItem('recordList') || '[]'))
+  },[])
+  useUpdate(()=>{
+    window.localStorage.setItem('recordList',JSON.stringify(recordList))
+  },recordList)
+  const addRecord=(newRecord:RecordItem)=>{
+    console.log(newRecord);
+    console.log(newRecord.amount);
+    console.log(newRecord.tagID);
+    if(newRecord.amount === 0){window.alert('请输入金额');return false}
+    if(newRecord.tagID === 0){window.alert('请选择标签');return false}
+    setRecordList([...recordList,newRecord])
+    return true
+  }
+  return {recordList,addRecord}
+}
+
+export {useRecords}
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,39 +5,45 @@ import {NotesSection} from './Money/NotesSection';
 import {NumberPadSection} from './Money/NumberPadSection';
 import {TagsSection} from './Money/TagsSection';
 import {TypesSection} from './Money/TypesSection';
+import {useRecords} from '../Hooks/useRecords';
 
 const MyLayout = styled(Layout)`
     display: flex;
     flex-direction: column;  
 `
-type Type = '-' | '+'
+
 // type Tag = {id:number,name:string}
 
-type Record = {
-  tagID:number,
-  note:string,
-  type:Type,
-  amount: number
+const defaultRecordItem = {
+  tagID: 0 ,
+  note: '',
+  type: '-' as Type,
+  amount: 0
 }
 
 const Money=()=>{
-  const [record,setRecord] = useState<Record>({
-    tagID: 0 ,
-    note: '',
-    type: '-' as Type,
-    amount: 0
-  })
-  const onChange = (obj:Partial<Record>)=>{
-    setRecord({...record, ...obj})
+  const [recordItem,setRecordItem] = useState(defaultRecordItem)
+  const {recordList,addRecord} = useRecords()
+  const onChange = (obj:Partial<RecordItem>)=>{
+    setRecordItem({...recordItem, ...obj})
+  }
+  const submit=()=>{
+    if(addRecord(recordItem)){
+      alert('保存成功')
+      setRecordItem(defaultRecordItem)
+    }else {
+      setRecordItem(defaultRecordItem)
+    }
   }
   return (
     <MyLayout>
-      <TagsSection value={record.tagID} onChange={(tagID)=>{onChange({tagID})}}/>
-      <NotesSection value={record.note} onChange={(note)=>{onChange({note})}}/>
-      <TypesSection value={record.type} onChange={(type)=>{onChange({type})}}/>
-      <NumberPadSection value={record.amount}
+      {JSON.stringify(recordList)}
+      <TagsSection value={recordItem.tagID} onChange={(tagID)=>{onChange({tagID})}}/>
+      <NotesSection value={recordItem.note} onChange={(note)=>{onChange({note})}}/>
+      <TypesSection value={recordItem.type} onChange={(type)=>{onChange({type})}}/>
+      <NumberPadSection value={recordItem.amount}
                         onChange={(amount)=>{onChange({amount})}}
-                        onOK={()=>{}}
+                        onOK={()=>{submit()}}
       />
     </MyLayout>
   )
